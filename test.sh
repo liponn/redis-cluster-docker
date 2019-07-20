882226fd5c6da3740cd7655796e0d66e2c30b362
wdy <13015531717@163.com>
2019-07-21 05:01:02 +0800

    init

--- test.sh
+++ test.sh
@@ -1,32 +1,44 @@
-MASTER_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redis-cluster_master_1)
-SLAVE_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redis-cluster_slave_1)
-SENTINEL_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redis-cluster_sentinel_1)
+#!/bin/bash
 
-echo Redis master: $MASTER_IP
-echo Redis Slave: $SLAVE_IP
+function docker_ip(){
+  docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $1
+}
+
+MASTER_IP=$(docker_ip redis-master)
+SLAVE_1_IP=$(docker_ip redis-slave-1)
+SLAVE_2_IP=$(docker_ip redis-slave-2)
+
+#echo 'init status:ip---container':
+#echo $MASTER_IP---redis-master
+#echo $SLAVE_1_IP---redis-slave-1
+#echo $SLAVE_2_IP---redis-slave-2
 echo ------------------------------------------------
-echo Initial status of sentinel
+echo Now status of sentinel
 echo ------------------------------------------------
-docker exec redis-cluster_sentinel_1 redis-cli -p 26379 info Sentinel
+docker exec redis-sentinel-1 redis-cli -p 26379 info Sentinel
 echo Current master is
-docker exec redis-cluster_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
+docker exec redis-sentinel-1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
 echo ------------------------------------------------
 
 echo Stop redis master
-docker pause redis-cluster_master_1
+echo $MASTER_IP---redis-master
+echo $SLAVE_1_IP---redis-slave-1
+echo $SLAVE_2_IP---redis-slave-2
+read -p "Which container do you want to restart?" -e CONTAINER
+docker pause $CONTAINER
 echo Wait for 10 seconds
 sleep 10
 echo Current infomation of sentinel
-docker exec redis-cluster_sentinel_1 redis-cli -p 26379 info Sentinel
+docker exec redis-sentinel-1 redis-cli -p 26379 info Sentinel
 echo Current master is
-docker exec redis-cluster_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
+docker exec redis-sentinel-1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
 
 
 echo ------------------------------------------------
 echo Restart Redis master
-docker unpause redis-cluster_master_1
+docker unpause $CONTAINER
 sleep 5
 echo Current infomation of sentinel
-docker exec redis-cluster_sentinel_1 redis-cli -p 26379 info Sentinel
+docker exec redis-sentinel-1 redis-cli -p 26379 info Sentinel
 echo Current master is
-docker exec redis-cluster_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
+docker exec redis-sentinel-1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
